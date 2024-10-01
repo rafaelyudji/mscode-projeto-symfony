8f96d07cd2a8c5d9ab206796b8a1714e231c7972
rafaelyudji <rafaelyudjig@gmail.com>
2024-10-01 19:37:28 -0300

    Corrigindo Erros

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,13 +4,14 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route(path: '/home', name: 'home')]
+    #[Route('/home', name: 'home')]
     public function index(): Response
     {
+        // Lógica para renderizar a página inicial
         return $this->render('home/index.html.twig');
-    }    
+    }
 }
---
+++ src/Security/LoginSuccessHandler.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+use Symfony\Component\Routing\RouterInterface;
+
+class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
+{
+    private RouterInterface $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token): Response
+    {
+        // Redireciona para a rota /home após o login
+        return new Response('', Response::HTTP_FOUND, ['Location' => $this->router->generate('home')]);
+    }
+}
